71b239d57f1070828b16ac408acc919c137998d2
Christopher Lambert <e1177@itsector.pt>
2021-01-31 19:36:50 +0000

    Inicio da validação CEDENTE

--- APIGeradorBoletos/Services/Validacao.cs
+++ APIGeradorBoletos/Services/Validacao.cs
@@ -24,24 +24,30 @@ namespace APIGerarBoletos.Services
 
         public static string ValidarBoleto(string vencimento, string valor)
         {
+            string msgError = "BOLETO: ";
             DateTime dateVencimento;
             if (!DateTime.TryParse(vencimento, out dateVencimento))
-                return "Boleto com Data de Vencimento Invalída ";
+                return msgError + "com Data de Vencimento Invalída ";
 
             Decimal deciValor;
             if (!Decimal.TryParse(valor, out deciValor))
-                return "Boleto com Valor Invalído ";
+                return msgError + "com Valor Invalído ";
 
             return string.Empty;
         }
 
         public static string ValidarCedente(CedenteIn cedenteIn)
         {
+            string msgError = "CEDENTE: ";
+            if (string.IsNullOrEmpty(cedenteIn.CNPJ) && string.IsNullOrEmpty(cedenteIn.CPF))
+                return msgError + "CPF ou CNPJ são obrigatorios ";
+
             return string.Empty;
         }
 
         public static string ValidarSacado(SacadoIn sacadoIn)
         {
+            string msgError = "SACADO: ";
             return string.Empty;
         }
     }
