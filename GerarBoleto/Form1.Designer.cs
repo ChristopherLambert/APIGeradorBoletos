597a95602bd0d8eb397efb9693de82e1fe0e3f66
Christopher Lambert <e1177@itsector.pt>
2021-02-03 09:44:58 +0000

    Add Versão Windows Forms

---
+++ GerarBoleto/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace GerarBoleto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtVencimento = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtSacado = new System.Windows.Forms.TextBox();
+            this.txtMorada = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.txtUF = new System.Windows.Forms.TextBox();
+            this.rdItau = new System.Windows.Forms.RadioButton();
+            this.rdBradesco = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gerar Boleto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtVencimento
+            // 
+            this.txtVencimento.Location = new System.Drawing.Point(44, 12);
+            this.txtVencimento.Name = "txtVencimento";
+            this.txtVencimento.Size = new System.Drawing.Size(138, 20);
+            this.txtVencimento.TabIndex = 1;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(282, 12);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(138, 20);
+            this.txtValor.TabIndex = 2;
+            // 
+            // txtSacado
+            // 
+            this.txtSacado.Location = new System.Drawing.Point(44, 53);
+            this.txtSacado.Name = "txtSacado";
+            this.txtSacado.Size = new System.Drawing.Size(138, 20);
+            this.txtSacado.TabIndex = 3;
+            // 
+            // txtMorada
+            // 
+            this.txtMorada.Location = new System.Drawing.Point(282, 53);
+            this.txtMorada.Name = "txtMorada";
+            this.txtMorada.Size = new System.Drawing.Size(138, 20);
+            this.txtMorada.TabIndex = 4;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.Location = new System.Drawing.Point(44, 92);
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(138, 20);
+            this.txtBairro.TabIndex = 5;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.Location = new System.Drawing.Point(282, 92);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(138, 20);
+            this.txtCidade.TabIndex = 6;
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(44, 130);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(138, 20);
+            this.txtCEP.TabIndex = 7;
+            // 
+            // txtUF
+            // 
+            this.txtUF.Location = new System.Drawing.Point(282, 130);
+            this.txtUF.Name = "txtUF";
+            this.txtUF.Size = new System.Drawing.Size(138, 20);
+            this.txtUF.TabIndex = 8;
+            // 
+            // rdItau
+            // 
+            this.rdItau.AutoSize = true;
+            this.rdItau.Location = new System.Drawing.Point(44, 169);
+            this.rdItau.Name = "rdItau";
+            this.rdItau.Size = new System.Drawing.Size(50, 17);
+            this.rdItau.TabIndex = 9;
+            this.rdItau.TabStop = true;
+            this.rdItau.Text = "ITAU";
+            this.rdItau.UseVisualStyleBackColor = true;
+            // 
+            // rdBradesco
+            // 
+            this.rdBradesco.AutoSize = true;
+            this.rdBradesco.Location = new System.Drawing.Point(44, 192);
+            this.rdBradesco.Name = "rdBradesco";
+            this.rdBradesco.Size = new System.Drawing.Size(70, 17);
+            this.rdBradesco.TabIndex = 10;
+            this.rdBradesco.TabStop = true;
+            this.rdBradesco.Text = "Bradesco";
+            this.rdBradesco.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 226);
+            this.Controls.Add(this.rdBradesco);
+            this.Controls.Add(this.rdItau);
+            this.Controls.Add(this.txtUF);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtBairro);
+            this.Controls.Add(this.txtMorada);
+            this.Controls.Add(this.txtSacado);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.txtVencimento);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtVencimento;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtSacado;
+        private System.Windows.Forms.TextBox txtMorada;
+        private System.Windows.Forms.TextBox txtBairro;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.TextBox txtUF;
+        private System.Windows.Forms.RadioButton rdItau;
+        private System.Windows.Forms.RadioButton rdBradesco;
+    }
+}
+
---
+++ GerarBoleto/Form1.cs
@@ -0,0 +1,147 @@
+﻿using BoletoNet;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GerarBoleto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtVencimento.Text = "30/12/2020";
+            txtValor.Text = "5890";
+            txtSacado.Text = "Christopher Lambert";
+            txtMorada.Text = "Bairro Novo - Curitiba";
+            txtBairro.Text = "Bairro Novo";
+            txtCidade.Text = "Curitiba";
+            txtCEP.Text = "81270-070";
+            txtUF.Text = "PR";
+            this.rdItau.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string vencimento = txtVencimento.Text;
+            String valorBoleto = txtValor.Text;
+            String numeroDocumento = "B20005446";
+
+            //cedente
+            String cedente_codigo = "1111111";
+            String cedente_nossoNumeroBoleto = "22222222";
+            String cedente_cpfCnpj = "123.456.789-01";
+            String cedente_nome = "CHRISTOPHER LAMBERT FRIGERIO DE SOUZA.";
+            String cedente_agencia = "1000";
+            String cedente_conta = "22507";
+            String cedente_digitoConta = "6";
+
+            //sacado
+            String sacado_cpfCnpj = "000.000.000-00";
+            String sacado_nome = txtSacado.Text;
+            String sacado_endereco = txtMorada.Text;
+            String sacado_bairro = txtBairro.Text;
+            String sacado_cidade = txtCidade.Text;
+            String sacado_cep = txtCEP.Text;
+            String sacado_uf = txtUF.Text;
+
+            Cedente cedente = new Cedente(cedente_cpfCnpj,
+            cedente_nome,
+            cedente_agencia,
+            cedente_conta,
+            cedente_digitoConta);
+            cedente.Codigo = Convert.ToInt32(cedente_codigo).ToString();
+
+            Boleto boleto = new Boleto(Convert.ToDateTime(vencimento),
+                   Convert.ToDecimal(valorBoleto),
+                   "109",
+                   cedente_nossoNumeroBoleto, cedente);
+            boleto.NumeroDocumento = numeroDocumento;
+
+            Sacado sacado = new Sacado(sacado_cpfCnpj, sacado_nome);
+            boleto.Sacado = sacado;
+            boleto.Sacado.Endereco.End = sacado_endereco;
+            boleto.Sacado.Endereco.Bairro = sacado_bairro;
+            boleto.Sacado.Endereco.Cidade = sacado_cidade;
+            boleto.Sacado.Endereco.CEP = sacado_cep;
+            boleto.Sacado.Endereco.UF = sacado_uf;
+
+            Instrucao_Itau instrucao = new Instrucao_Itau();
+            instrucao.Descricao = "Não Receber após o vencimento";
+
+            boleto.Instrucoes.Add(instrucao);
+
+            EspecieDocumento_Itau especieItau = new EspecieDocumento_Itau("99");
+            EspecieDocumento_Bradesco especieBradesco = new EspecieDocumento_Bradesco("16");
+
+            BoletoBancario boleto_bancario = new BoletoBancario();
+            // ITAU ou BRADESCO
+            if (this.rdItau.Checked)
+            {
+                boleto.EspecieDocumento = especieItau;
+                boleto_bancario.CodigoBanco = 341;
+            }
+            else if (this.rdBradesco.Checked)
+            {
+                boleto.EspecieDocumento = especieBradesco;
+                boleto_bancario.CodigoBanco = 237;
+                //boleto.Carteira = "02";
+                //boleto.Carteira = "19";
+                //boleto_bancario = GerarBoletoCarteira16();
+            }
+                
+            //boleto_bancario.Boleto = boleto;
+            boleto_bancario.MostrarCodigoCarteira = true;
+            //boleto_bancario.Boleto.Valida();
+            boleto_bancario.MostrarComprovanteEntrega = true;
+
+            try
+            {
+                SaveBoletoPDF(boleto_bancario.MontaBytesPDF());
+                GerarRemessa(cedente, boleto);
+                MessageBox.Show("Boleto Gerado Com Sucesso em C:/boleto.pdf");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Falha ao gerar o Arquivo -- " + ex.Message);
+            }
+          
+        }
+
+        private void SaveBoletoPDF(byte[] boletoPdf)
+        {
+            MemoryStream stream = new MemoryStream();
+            stream.Write(boletoPdf, 0, boletoPdf.Length);
+
+            var fileStream = File.Create("C:\\boleto.pdf");
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.CopyTo(fileStream);
+            fileStream.Close();
+        }
+        private void GerarRemessa(Cedente cedente, Boleto boleto)
+        {
+            //Gerar Remessa
+            Boletos boletos = new Boletos();
+            boletos.Add(boleto);
+
+            var objRemessa = new ArquivoRemessa(TipoArquivo.CNAB400);
+            var memoryStr = new MemoryStream();
+            objRemessa.GerarArquivoRemessa("09", new Banco(341), cedente, boletos, memoryStr, 1000);
+        }
+
+        private BoletoBancario GerarBoletoCarteira16() // Bradesco
+        {
+            var vencimento = new DateTime(2015, 7, 20);
+            var cedente = new Cedente("00.000.000/0000-00", "Empresa Teste", "0413", "8", "0002916", "5");
+            var boleto = new Boleto(vencimento, 123, "16", "00970171092", cedente);
+            boleto.NumeroDocumento = "970171092";
+
+            var boletoBancario = new BoletoBancario();
+            boletoBancario.CodigoBanco = 237;
+            boletoBancario.Boleto = boleto;
+
+            return boletoBancario;
+        }
+    }
+}
