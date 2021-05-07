28ed993f61d5fc5c4ce1f535d2ac326b05882782
Christopher Souza <christopherwhb@gmail.com>
2021-05-07 22:21:09 +0100

    LoginController

---
+++ APIGeradorBoletos/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using APIGerarBoletos.Models;
+using APIGerarBoletos.Services;
+using System;
+using System.Web.Http;
+
+namespace APIGeradorBoletos.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        public string Login(string user, string password)
+        {
+            return "Logado com Sucesso";
+        }
+    }
+}
